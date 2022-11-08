f932e1920e036a1ea28a566294da5ee2065b27e7
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-08 16:25:27 -0500

    Crear proyecto backend del e-commerce Sejmet Whip's Store
    Creacion del proyecto backend empleando el Spring Boot para el e-commerce Sejmet Whip's Store

---
+++ src/main/java/com/project/sejmet/SejmetApplication.java
@@ -0,0 +1,13 @@
+package com.project.sejmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejmetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sejmet/SejmetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sejmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
