fe0c5931ca73fa091a91f94cad938f1fccb4151a
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-22 15:59:37 -0500

    Create SaleProduct entity

---
+++ src/main/java/com/project/sejmet/entities/SaleProduct.java
@@ -0,0 +1,34 @@
+package com.project.sejmet.entities;
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="sale_product")
+@Data
+public class SaleProduct implements Serializable{
+    @Id
+    @SequenceGenerator(name="sale_product_id_seq", allocationSize=1)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="sale_product_id_seq")
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name="sale_id")
+    private Sale sale;
+
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+
+    private int productAmount;
+}
