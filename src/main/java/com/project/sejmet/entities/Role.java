b4395af5d5f90172771001e840af8a28b0f3fa25
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-27 20:48:53 -0500

    Update Role.java

--- src/main/java/com/project/sejmet/entities/Role.java
+++ src/main/java/com/project/sejmet/entities/Role.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Column;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -20,5 +21,6 @@ public class Role implements Serializable{
     @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="role_id_seq")
     private int id;
 
+    @Column(name = "role_name")
     private String roleName;
 }
