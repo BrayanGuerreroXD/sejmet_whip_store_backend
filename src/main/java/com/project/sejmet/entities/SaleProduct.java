499b3f213d919fdf5fad404370e62f33119f4ae1
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-27 20:46:47 -0500

    Update SaleProduct.java

--- src/main/java/com/project/sejmet/entities/SaleProduct.java
+++ src/main/java/com/project/sejmet/entities/SaleProduct.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Column;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -29,5 +30,6 @@ public class SaleProduct implements Serializable{
     @JoinColumn(name="product_id")
     private Product product;
 
+    @Column(name = "product_amount")
     private int productAmount;
 }
