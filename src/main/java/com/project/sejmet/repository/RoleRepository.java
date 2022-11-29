c1cdd4f96d99eec9effed02dea01e90aeb69494d
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-29 18:49:37 -0500

    Create RoleRepository.java

---
+++ src/main/java/com/project/sejmet/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.project.sejmet.repository;
+
+import com.project.sejmet.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
