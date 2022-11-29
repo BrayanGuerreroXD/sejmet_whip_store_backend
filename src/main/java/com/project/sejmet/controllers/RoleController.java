d114dea77c52ddfe6c4954f3f1018b6cec628282
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-29 18:53:42 -0500

    Update RoleController.java

--- src/main/java/com/project/sejmet/controllers/RoleController.java
+++ src/main/java/com/project/sejmet/controllers/RoleController.java
@@ -1,5 +1,7 @@
 package com.project.sejmet.controllers;
 
+import com.project.sejmet.entities.Role;
+import com.project.sejmet.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,5 +21,63 @@ import java.util.Optional;
 @RequestMapping("/roles")
 public class RoleController {
 
-    
+    @Autowired
+    RoleRepository roleRepository;
+
+
+    @GetMapping
+    public List<Role> getRoleAll() {
+        return roleRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public Role getRolebyId(@PathVariable int id) {
+
+        Optional<Role> role = roleRepository.findById(id);
+
+        if (role.isPresent()) {
+            return role.get();
+        }
+
+        return null;
+    }
+
+
+    @PostMapping
+    public Role postRole(@RequestBody Role role) {
+        roleRepository.save(role);
+        return role;
+    }
+
+
+    @PutMapping("/{id}")
+    public Role putRolebyId(@PathVariable int id, @RequestBody Role role) {
+
+        Optional<Role> roleCurrent = roleRepository.findById(id);
+
+        if (roleCurrent.isPresent()) {
+            Role roleReturn = roleCurrent.get();
+            roleReturn.setRoleName(role.getRoleName());
+            roleRepository.save(roleReturn);
+            return roleReturn;
+        }
+
+        return null;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public Role deleteRolebyId(@PathVariable int id) {
+
+        Optional<Role> role = roleRepository.findById(id);
+
+        if (role.isPresent()) {
+            Role roleReturn = role.get();
+            roleRepository.deleteById(id);
+            return roleReturn;
+        }
+
+        return null;
+    }
 }
