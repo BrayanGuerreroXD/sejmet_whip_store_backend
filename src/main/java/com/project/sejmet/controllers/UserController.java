13aaa301ec68f28b8ef6b5c4d2438a4f22793c97
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-29 18:38:00 -0500

    Update UserController.java

--- src/main/java/com/project/sejmet/controllers/UserController.java
+++ src/main/java/com/project/sejmet/controllers/UserController.java
@@ -3,11 +3,20 @@ package com.project.sejmet.controllers;
 import com.project.sejmet.entities.User;
 import com.project.sejmet.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
