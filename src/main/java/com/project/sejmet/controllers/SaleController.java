fa902e0a7029826170b9ff391d1324847947a416
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-29 18:47:11 -0500

    Update SaleController.java

--- src/main/java/com/project/sejmet/controllers/SaleController.java
+++ src/main/java/com/project/sejmet/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.project.sejmet.controllers;
 
+import com.project.sejmet.entities.Sale;
+import com.project.sejmet.repository.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,4 +20,69 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/sales")
 public class SaleController {
+
+    @Autowired
+    SaleRepository saleRepository;
+
+
+    @GetMapping
+    public List<Sale> getSaleAll() {
+        return saleRepository.findAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public Sale getSalebyId(@PathVariable int id) {
+
+        Optional<Sale> sale = saleRepository.findById(id);
+
+        if (sale.isPresent()) {
+            return sale.get();
+        }
+
+        return null;
+    }
+
+
+    @PostMapping
+    public Sale postSale(@RequestBody Sale sale) {
+        saleRepository.save(sale);
+        return sale;
+    }
+
+
+    @PutMapping("/{id}")
+    public Sale putSalebyId(@PathVariable int id, @RequestBody Sale sale) {
+
+        Optional<Sale> saleCurrent = saleRepository.findById(id);
+
+        if (saleCurrent.isPresent()) {
+            Sale saleReturn = saleCurrent.get();
+
+            saleReturn.setUser(sale.getUser());
+            saleReturn.setSaleDate(sale.getSaleDate());
+            saleReturn.setDeliveryDate(sale.getDeliveryDate());
+            saleReturn.setTotalPrice(sale.getTotalPrice());
+
+            saleRepository.save(saleReturn);
+            return saleReturn;
+        }
+
+        return null;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public Sale deleteSalebyId(@PathVariable int id) {
+
+        Optional<Sale> sale = saleRepository.findById(id);
+
+        if (sale.isPresent()) {
+            Sale saleReturn = sale.get();
+            saleRepository.deleteById(id);
+            return saleReturn;
+        }
+
+        return null;
+    }
 }
