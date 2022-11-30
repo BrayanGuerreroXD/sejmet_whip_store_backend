c6ac806404e73f2cf8b2a86ba68ffd2e3ca44210
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-29 19:25:19 -0500

    Update QuestionRepository.java

--- src/main/java/com/project/sejmet/repository/QuestionRepository.java
+++ src/main/java/com/project/sejmet/repository/QuestionRepository.java
@@ -1,4 +1,7 @@
 package com.project.sejmet.repository;
 
-public interface QuestionRepository {
+import com.project.sejmet.entities.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
 }
