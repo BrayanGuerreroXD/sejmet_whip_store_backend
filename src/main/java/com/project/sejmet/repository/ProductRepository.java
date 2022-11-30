ff56b69f38e4e285e19dc264a390b99a8e0c1163
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-29 20:54:44 -0500

    Update ProductRepository.java

--- src/main/java/com/project/sejmet/repository/ProductRepository.java
+++ src/main/java/com/project/sejmet/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.project.sejmet.repository;
 
-public interface ProductRepository {
+import com.project.sejmet.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
