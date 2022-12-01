d230d0822c8d1413291ac96a233f866130acef0a
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-30 22:36:06 -0500

    Update ImageController.java

--- src/main/java/com/project/sejmet/controllers/ImageController.java
+++ src/main/java/com/project/sejmet/controllers/ImageController.java
@@ -26,7 +26,7 @@ public class ImageController {
 
 
     @GetMapping
-    public List<Image> getRoleAll() {
+    public List<Image> getImageAll() {
         return imageRepository.findAll();
     }
 
