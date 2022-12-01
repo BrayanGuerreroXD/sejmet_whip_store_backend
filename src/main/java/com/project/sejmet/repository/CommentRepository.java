d62983ffa5a818bbd3549363318ae8d3323cbecb
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-30 22:20:07 -0500

    Update CommentRepository.java

--- src/main/java/com/project/sejmet/repository/CommentRepository.java
+++ src/main/java/com/project/sejmet/repository/CommentRepository.java
@@ -1,4 +1,7 @@
 package com.project.sejmet.repository;
 
-public interface CommentRepository {
+import com.project.sejmet.entities.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
 }
