59d8927577ed55958f0c874e7961c59d21ec497f
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-30 22:06:29 -0500

    Update ImageRepository.java

--- src/main/java/com/project/sejmet/repository/ImageRepository.java
+++ src/main/java/com/project/sejmet/repository/ImageRepository.java
@@ -1,4 +1,7 @@
 package com.project.sejmet.repository;
 
-public interface ImageRepository {
+import com.project.sejmet.entities.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Integer> {
 }
